93050c095d1a8e6980edda17f46e9cbf8008cef2
dafma <mar_tp@hotmail.es>
2017-02-08 14:42:12 -0600

    personalizando snackbar

--- app/src/main/java/com/example/seader/petagram/MainActivity.java
+++ app/src/main/java/com/example/seader/petagram/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -22,7 +23,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
-                Snackbar.make(v, getResources().getString(R.string.mensaje), Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(v, getResources().getString(R.string.mensaje), Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v){
+                                Log.i("SNACK BAR", "Click en snack bar");
+                            }
+                        } ).show();
             }
         });
     }
