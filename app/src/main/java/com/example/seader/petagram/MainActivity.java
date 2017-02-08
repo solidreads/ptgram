8fe955f791481db614677916629953b451a4fcc6
dafma <mar_tp@hotmail.es>
2017-02-08 14:26:51 -0600

    agregado boton floating con efecto toast

--- app/src/main/java/com/example/seader/petagram/MainActivity.java
+++ app/src/main/java/com/example/seader/petagram/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.seader.petagram;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+    public void agregarFAB(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
+        //seimpre va a estar alerta a lo que se escuche
+        miFAB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
