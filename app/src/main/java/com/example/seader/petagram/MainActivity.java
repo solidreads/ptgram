0cf2d548f84e3e613e69dbc84d49fd98e61a6919
dafma <mar_tp@hotmail.es>
2017-02-08 20:41:27 -0600

    swipe refresh + list view

--- app/src/main/java/com/example/seader/petagram/MainActivity.java
+++ app/src/main/java/com/example/seader/petagram/MainActivity.java
@@ -2,20 +2,51 @@ package com.example.seader.petagram;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import static com.example.seader.petagram.R.id.lstMiLista;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMiLista;
+    RecyclerView.Adapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        sfiMiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+                @Override
+                public void onRefresh(){
+                    refrescandoContenido();
+        }
+        });
     }
+
+    public void refrescandoContenido(){
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        sfiMiIndicadorRefresh.setRefreshing(false);
+    }
+
     public void agregarFAB(){
         FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
         //seimpre va a estar alerta a lo que se escuche
